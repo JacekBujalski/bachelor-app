b8be3143b2e538c883cd35386914c5150134b76f
Jacek Bujalski <jacekbujalski@yahoo.com>
2021-11-02 00:32:21 +0100

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./components/Dashboard";
+
 function App() {
+  const { userRole, adminRole, managerRole, isLoggedIn } = useSelector(
+    (state) => state.user
+  );
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+      {isLoggedIn ? (
+        (userRole && <Redirect to="/user" />) ||
+        (adminRole && <Redirect to="/dashboard" />) ||
+        (managerRole && <Redirect to="/dashboard" />)
+      ) : (
+        <Redirect to="/login" />
+      )}
     </div>
   );
 }
