2fd8d0e028d23edb79683ad95da80706a0d82f07
Jacek Bujalski <jacekbujalski@yahoo.com>
2021-11-03 01:06:18 +0100

    added form for updating user

--- src/components/admin/user/user.js
+++ src/components/admin/user/user.js
@@ -8,7 +8,6 @@ import { useParams } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flex: 4,
     padding: theme.spacing(1),
   },
   userTitleContainer: {
@@ -78,11 +77,74 @@ const useStyles = makeStyles((theme) => ({
   userShowInfoTitle: {
     marginLeft: theme.spacing(2),
   },
+  userEditLeft: {
+    flex: 2,
+  },
+  userEditTitle: {
+    fontSize: "1.4rem",
+    fontWeight: "600",
+  },
+  userEditForm: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: theme.spacing(4),
+  },
+  userEditItem: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: theme.spacing(4),
+    "& label": {
+      marginBottom: theme.spacing(2),
+      fontSize: "1.1rem",
+    },
+  },
+  userEditInput: {
+    border: "none",
+    fontSize: "1.2rem",
+    width: "15vw",
+    height: 30,
+    borderBottom: "1px solid gray",
+  },
+  userEditImg: {
+    width: 250,
+    height: 250,
+  },
+  userEditRight: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  userUpdateButton: {
+    border: "none",
+    borderRadius: "5px",
+    padding: theme.spacing(1),
+    fontSize: "1rem",
+    backgroundColor: "#ff0000",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#e80606",
+    },
+  },
 }));
 
 export default function User(props) {
   const [data, setData] = useState({});
+  const [inputData, setInputData] = useState({});
   const classes = useStyles();
+
+  const handleChange = (prop) => (event) => {
+    setInputData({ ...inputData, [prop]: event.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInputData({
+      ...inputData,
+      userId: inputData.user.idUsers,
+      companyId: inputData.company.idCompany,
+    });
+    console.log(inputData);
+  };
 
   const params = useParams();
   const idUser = params.id;
@@ -91,7 +153,7 @@ export default function User(props) {
     axios.get(API_URL + "usersData/" + idUser).then((response) => {
       const users = response.data;
       setData(users);
-      console.log(users);
+      setInputData(users);
     });
   };
 
@@ -106,7 +168,7 @@ export default function User(props) {
         <Button className={classes.userAddButton}>Dodaj użytkownika</Button>
       </div>
       <div className={classes.userContainer}>
-        <Paper className={classes.userDisplay}>
+        <Paper className={classes.userDisplay} elevation={5}>
           <div className={classes.userDisplayTop}>
             <span className={classes.userDisplayUsername}>
               {data.firstName + ` ` + data.lastName}
@@ -143,7 +205,56 @@ export default function User(props) {
             </div>
           </div>
         </Paper>
-        <Paper className={classes.userEdit}></Paper>
+        <Paper className={classes.userEdit} elevation={5}>
+          <span className={classes.userEditTitle}>Edycja danych</span>
+          <form className={classes.userEditForm} onSubmit={handleSubmit}>
+            <div className={classes.userEditLeft}>
+              <div className={classes.userEditItem}>
+                <label> Imie </label>
+                <input
+                  name={"firstName"}
+                  type="text"
+                  placeholder={data.firstName}
+                  defaultValue={inputData.firstName}
+                  className={classes.userEditInput}
+                  onChange={handleChange("firstName")}
+                />
+              </div>
+              <div className={classes.userEditItem}>
+                <label> Nazwisko </label>
+                <input
+                  name={"lastName"}
+                  type="text"
+                  placeholder={data.lastName}
+                  defaultValue={inputData.lastName}
+                  className={classes.userEditInput}
+                  onChange={handleChange("lastName")}
+                />
+              </div>
+              <div className={classes.userEditItem}>
+                <label> Numer Telefonu </label>
+                <input
+                  name={"phoneNumber"}
+                  type="text"
+                  placeholder={data.phoneNumber}
+                  defaultValue={inputData.phoneNumber}
+                  className={classes.userEditInput}
+                  onChange={handleChange("phoneNumber")}
+                />
+              </div>
+            </div>
+            <div className={classes.userEditRight}>
+              <img
+                className={classes.userEditImg}
+                src={process.env.PUBLIC_URL + "/assets/pic.png"}
+                alt={""}
+              />
+              <button type={"submit"} className={classes.userUpdateButton}>
+                Aktualizuj
+              </button>
+            </div>
+          </form>
+        </Paper>
       </div>
     </div>
   );
