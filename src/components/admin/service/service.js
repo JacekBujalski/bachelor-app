ca5b3e0902d25ab2e5a138326416feb9c0f81c4a
Jacek Bujalski <jacekbujalski@yahoo.com>
2021-11-17 15:17:19 +0100

    removed service edit and add component

--- src/components/admin/service/service.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Service(props) {
-  return <div></div>;
-}
-
-export default Service;
--- src/components/admin/service/serviceAdd.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ServiceAdd(props) {
-  return <div></div>;
-}
-
-export default ServiceAdd;
