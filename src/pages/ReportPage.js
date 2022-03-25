359613c597ff5b14e41d3cfec69cd5b72dfabf70
Jacek Bujalski <jacekbujalski@yahoo.com>
2022-03-25 18:52:05 +0100

    changed display of reports page

--- src/pages/ReportPage.js
+++ src/pages/ReportPage.js
@@ -2,11 +2,20 @@ import React, { useEffect, useState } from "react";
 import { makeStyles, Paper } from "@material-ui/core";
 import axios from "axios";
 import { API_URL } from "../API/api";
-import { Route, Switch } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import moment from "moment";
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import { pl } from "date-fns/locale";
+import DateFnsUtils from "@date-io/date-fns";
 
 const useStyles = makeStyles((theme) => ({
   selectContainer: {
     padding: theme.spacing(2),
+    width: "50%",
+    margin: "auto",
   },
   item: {
     display: "flex",
@@ -34,15 +43,33 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "1.4rem",
     color: "#d32f2f",
   },
+  generateButton: {
+    border: "none",
+    borderRadius: "5px",
+    marginTop: theme.spacing(4),
+    padding: theme.spacing(2),
+    fontSize: "1rem",
+    backgroundColor: "#ff0000",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#e80606",
+    },
+  },
 }));
 
 export default function ReportPage(props) {
+  let history = useHistory();
   const classes = useStyles();
   const [companies, setCompanies] = useState([]);
   const [showCarSelect, setShowCarSelect] = useState(false);
   const [cars, setCars] = useState({});
   const [values, setValues] = useState({});
-  const reportType = ["Raport tankowań", "Raport uszkodzeń", "Raport serwisów"];
+  const [dateFrom, setDateFrom] = useState();
+  const [dateTo, setDateTo] = useState();
+  const [dates, setDates] = useState({
+    dateFrom: moment(dateFrom).format("yyyy-MM-DD"),
+    dateTo: moment(dateTo).format("yyyy-MM-DD"),
+  });
 
   const getCompaniesList = () => {
     axios.get(API_URL + "company/").then((response) => {
@@ -54,7 +81,6 @@ export default function ReportPage(props) {
   const getCarsList = (companyId) => {
     axios.get(API_URL + "company/" + companyId + "/cars").then((response) => {
       const data = response.data;
-      console.log(data);
       setCars(data);
       if (Array.isArray(data) && data.length) {
         setShowCarSelect(true);
@@ -78,59 +104,126 @@ export default function ReportPage(props) {
     setValues({ ...values, [prop]: event.target.value });
   };
 
+  const handleDateFromChange = (date) => {
+    setDateFrom(date);
+  };
+
+  const handleDateToChange = (date) => {
+    setDateTo(date);
+  };
+
+  useEffect(() => {
+    let formattedDateFrom = moment(dateFrom).format("yyyy-MM-DD");
+    setDates({ ...dates, dateFrom: formattedDateFrom });
+  }, [dateFrom]);
+
+  useEffect(() => {
+    let formattedDate = moment(dateTo).format("yyyy-MM-DD");
+    setDates({ ...dates, dateTo: formattedDate });
+  }, [dateTo]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    localStorage.setItem("dateTo", dates.dateTo);
+    localStorage.setItem("dateFrom", dates.dateFrom);
+
+    if (values.reportType === "fueling") {
+      history.push("/dashboard/reports/fueling/" + values.carId);
+    } else if (values.reportType === "services") {
+      history.push("/dashboard/reports/services/" + values.carId);
+    } else if (values.reportType === "damages") {
+      history.push("/dashboard/reports/damages/" + values.carId);
+    }
+  };
+
   return (
     <Paper elevation={3} className={classes.selectContainer}>
-      <div className={classes.item}>
-        <label>Wybierz firmę</label>
-        <select
-          className={classes.itemSelect}
-          onChange={handleChange("companyId")}
-        >
-          <option> Wybierz firmę</option>
-          {companies.map((company) => (
-            <option key={company.idCompany} value={company.idCompany}>
-              {company.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      {showCarSelect && cars ? (
-        <>
-          <div className={classes.item}>
-            <label>Wybierz samochód</label>
-            <select
-              className={classes.itemSelect}
-              onChange={handleChange("carId")}
-            >
-              <option> Wybierz samochód</option>
-              {cars.map((car) => (
-                <option key={car.idCar} value={car.idCar}>
-                  {`${car.carBrand} ${car.carModel} ${car.plateNumber}`}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={classes.item}>
-            <label>Wybierz typ raportu</label>
-            <select
-              className={classes.itemSelect}
-              onChange={handleChange("reportType")}
-            >
-              <option> Wybierz typ raportu</option>
-              {reportType.map((type) => (
-                <option key={type} value={type}>
-                  {type}
-                </option>
-              ))}
-              )}
-            </select>
-          </div>
-        </>
-      ) : (
-        <>
-          <h1 className={classes.errorMessage}> Brak samochodów w firmie</h1>
-        </>
-      )}
+      <form onSubmit={handleSubmit}>
+        <div className={classes.item}>
+          <label>Wybierz firmę</label>
+          <select
+            className={classes.itemSelect}
+            onChange={handleChange("companyId")}
+          >
+            <option> Wybierz firmę</option>
+            {companies.map((company) => (
+              <option key={company.idCompany} value={company.idCompany}>
+                {company.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        {showCarSelect && cars ? (
+          <>
+            <div className={classes.item}>
+              <label>Wybierz samochód</label>
+              <select
+                className={classes.itemSelect}
+                onChange={handleChange("carId")}
+              >
+                <option> Wybierz samochód</option>
+                {cars.map((car) => (
+                  <option key={car.idCar} value={car.idCar}>
+                    {`${car.carBrand} ${car.carModel} ${car.plateNumber}`}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className={classes.item}>
+              <label>Wybierz typ raportu</label>
+              <select
+                className={classes.itemSelect}
+                onChange={handleChange("reportType")}
+              >
+                <option> Wybierz typ raportu</option>
+                <option value={"fueling"}> Raport tankowań</option>
+                <option value={"services"}> Raport serwisowy</option>
+                <option value={"damages"}> Raport uszkodzeń</option>
+              </select>
+            </div>
+            <div className={classes.item}>
+              <label>Data od</label>
+              <MuiPickersUtilsProvider locale={pl} utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  autoOk
+                  cancelLabel={"Anuluj"}
+                  variant="dialog"
+                  inputVariant="outlined"
+                  format="dd.MM.yyyy"
+                  value={dateFrom}
+                  InputAdornmentProps={{ position: "start" }}
+                  onChange={(date) => handleDateFromChange(date)}
+                />
+              </MuiPickersUtilsProvider>
+            </div>
+            <div className={classes.item}>
+              <label>Data do</label>
+              <MuiPickersUtilsProvider locale={pl} utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  autoOk
+                  cancelLabel={"Anuluj"}
+                  variant="dialog"
+                  inputVariant="outlined"
+                  format="dd.MM.yyyy"
+                  value={dateTo}
+                  InputAdornmentProps={{ position: "start" }}
+                  onChange={(date) => handleDateToChange(date)}
+                />
+              </MuiPickersUtilsProvider>
+            </div>
+            <div className={classes.item}>
+              <button type="submit" className={classes.generateButton}>
+                Dalej >
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <h1 className={classes.errorMessage}> Brak samochodów w firmie</h1>
+          </>
+        )}
+      </form>
     </Paper>
   );
 }
